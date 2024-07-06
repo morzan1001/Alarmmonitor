1cec112b1e4353a5e7a40514918f0678e1051cf8
Matthias Duve <morzan1001@mail.runforest.run>
2024-07-06 21:28:35 +0200

    changed json to thilos requirements

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -18,22 +18,24 @@ L.Icon.Default.mergeOptions({
 const WebSocketURL = 'wss://116.203.84.208:8000/ws';
 
 interface MapData {
-  Latitude: number;
-  Longitude: number;
+  coordinates: {
+    lat: number;
+    lng: number;
+  }
 }
 
 interface DashboardData {
-  map_data: MapData;
-  topic: string;
+  ziel: MapData;
+  keyword: string;
   description: string;
-  table_content: string[];
+  vehicles: string[];
 }
 
-const MapUpdater: React.FC<{ coordinates: MapData }> = ({ coordinates }) => {
+const MapUpdater: React.FC<{ destination: MapData }> = ({ destination }) => {
   const map = useMap();
   useEffect(() => {
-    map.setView([coordinates.Latitude, coordinates.Longitude], 16);
-  }, [coordinates, map]);
+    map.setView([destination.coordinates.lng, destination.coordinates.lat], 16);
+  }, [destination, map]);
 
   return null;
 };
@@ -43,11 +45,11 @@ const Dashboard: React.FC = () => {
     Latitude: 50.761952,
     Longitude: 7.049516,
   });
-  const [topic, setTopic] = useState<string>('Alarmstichwort');
+  const [keyword, setKeyword] = useState<string>('Alarmstichwort');
   const [description, setDescription] = useState<string>(
     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum maiores atque sunt possimus nisi id fugiat pariatur, architecto laboriosam. Necessitatibus itaque, assumenda id nobis consectetur tempore placeat at inventore totam.'
   );
-  const [tableContent, setTableContent] = useState<string[]>([
+  const [vehicles, setTableContent] = useState<string[]>([
     'Fahrzeug 1',
     'Fahrzeug 2',
   ]);
@@ -63,10 +65,10 @@ const Dashboard: React.FC = () => {
       try {
         const data: DashboardData = JSON.parse(event.data);
         console.log('Parsed data:', data);
-        setCoordinates(data.map_data);
-        setTopic(data.topic);
+        setCoordinates(data.ziel.coordinates);
+        setKeyword(data.keyword);
         setDescription(data.description);
-        setTableContent(data.table_content);
+        setTableContent(data.vehicles);
       } catch (error) {
         console.error('Error parsing WebSocket message:', error);
       }
@@ -89,21 +91,21 @@ const Dashboard: React.FC = () => {
     <div className='flex h-screen bg-gray-900 text-white'>
       <div className='w-1/2 h-full'>
         <MapContainer
-          center={[coordinates.Latitude, coordinates.Longitude]}
+          center={[destination.coordinates.lat, destination.coordinates.lng]}
           zoom={13}
           className='h-full w-full'
         >
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-          <Marker position={[coordinates.Latitude, coordinates.Longitude]}>
+          <Marker position={[destination.coordinates.lat, destination.coordinates.lng]}>
             <Popup>
-              Coordinates: {coordinates.Latitude}, {coordinates.Longitude}
+              Coordinates: {destination.coordinates.lat}, {destination.coordinates.lng}
             </Popup>
           </Marker>
           <MapUpdater coordinates={coordinates} />
         </MapContainer>
       </div>
       <div className='w-1/2 p-8 flex flex-col justify-center items-center'>
-        <h1 className='text-4xl mb-12 font-bold'>{topic}</h1>
+        <h1 className='text-4xl mb-12 font-bold'>{keyword}</h1>
         <p className='mb-10 font-extralight text-lg'>{description}</p>
         <table className='w-full table-auto my-6'>
           <thead>
@@ -112,7 +114,7 @@ const Dashboard: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {tableContent.map((item, index) => (
+            {vehicles.map((item, index) => (
               <tr key={index}>
                 <td>
                   <div className='px-4 py-2'>{item}</div>
